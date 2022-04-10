01565747a693679bf2fe24e9ea718dbedf117bcf
jaikumarPristynCare <jai.kumar@pristyncare.com>
2022-04-11 00:04:06 +0530

    navbar fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -15,6 +15,33 @@ export class NavbarComponent implements OnInit {
     document.getElementById(id)?.scrollIntoView({behavior:'smooth'});
   }
   showNavbar: boolean = false;
+  runningTimeOut: any;
   selectedBtn = 'profile'
-
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if(this.runningTimeOut){
+      clearTimeout(this.runningTimeOut);
+    }
+    if(window.scrollY>=0.8*window.innerHeight){
+      this.showNavbar = true;
+      this.runningTimeOut = setTimeout(() => {
+        this.showNavbar = false
+      }, 5000);
+    }
+    else{
+      this.showNavbar =  false;
+    }
+    if(window.scrollY>=window.innerHeight){
+      this.selectedBtn = 'profile';
+    }
+    if(window.scrollY>=1.8*window.innerHeight){
+      this.selectedBtn = 'experience';
+    }
+    if(window.scrollY>=2.8*window.innerHeight){
+      this.selectedBtn = 'skills';
+    }
+    if(window.scrollY>=3.8*window.innerHeight){
+      this.selectedBtn = 'contact';
+    }
+  }
 }
